18b650bb07223a7b8676b323c9317db08874fdb6
Yoorwan <74179029+Yoorwan@users.noreply.github.com>
2021-10-14 11:36:06 +0200

    Merge x4

---
+++ Source/Projet2/SpawnerOfIA.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SpawnerOfIA.h"
+#include "Projet2GameMode.h"
+#include "AICharacterTestP.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ASpawnerOfIA::ASpawnerOfIA()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+ 
+	BoxCollision -> OnComponentBeginOverlap.AddDynamic(this, &ASpawnerOfIA::OnBoxBeginOverlap);
+
+}
+
+// Called when the game starts or when spawned
+void ASpawnerOfIA::BeginPlay()
+{
+	Super::BeginPlay();
+	AProjet2GameMode* Gamemode = Cast<AProjet2GameMode>(GetWorld()->GetAuthGameMode());
+	Gamemode->SpawnerIA = this;
+	Gamemode->MakeCheckForSpawn();
+}
+
+void ASpawnerOfIA::OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AAICharacterTestP* charaIA = Cast<AAICharacterTestP>(OtherActor);
+	if (charaIA != nullptr)
+	{
+		if (charaIA->GoBackToZone == true)
+		{
+			charaIA->Destroy();
+			AProjet2GameMode* Gamemode = Cast<AProjet2GameMode>(GetWorld()->GetAuthGameMode());
+			Gamemode->NumberOfEnnemySpawn--;
+			Gamemode->MakeCheckForSpawn();
+		}
+	}
+}
+
+// Called every frame
+void ASpawnerOfIA::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Projet2/SpawnerOfIA.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnerOfIA.generated.h"
+
+UCLASS()
+class PROJET2_API ASpawnerOfIA : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnerOfIA();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	class UBoxComponent* BoxCollision;
+	
+	
+	UFUNCTION()
+	void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
