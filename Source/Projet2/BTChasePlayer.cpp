38108f85f8f2a7ee8593a9e6341f9518fac0dc48
Yoorwan <74179029+Yoorwan@users.noreply.github.com>
2021-10-22 11:50:28 +0200

    Merge

--- Source/Projet2/BTChasePlayer.cpp
+++ Source/Projet2/BTChasePlayer.cpp
@@ -5,11 +5,13 @@
 #include "MyAIControllerTestP.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "Kismet/GameplayStatics.h"
 
 EBTNodeResult::Type UBTChasePlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	UBlackboardComponent* bbComp = OwnerComp.GetBlackboardComponent();
 	AMyAIControllerTestP* controller = Cast<AMyAIControllerTestP>(OwnerComp.GetAIOwner());
-	FVector playerLocation = controller->GetBlackboardComp()->GetValueAsVector("JoueurPos");
+	FVector playerLocation = bbComp->GetValueAsVector("JoueurPos");
 	OwnerComp.GetBlackboardComponent()->SetValueAsBool("WasChasing", true);
 
 	UAIBlueprintHelperLibrary::SimpleMoveToLocation(controller, playerLocation);
