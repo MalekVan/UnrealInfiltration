845dc95c84de15adb603dcb3cc6f7d19c83e71cb
Yoorwan <74179029+Yoorwan@users.noreply.github.com>
2021-11-03 18:00:04 +0100

    poush

---
+++ Source/Projet2/NotAGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "NotAGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJET2_API ANotAGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
